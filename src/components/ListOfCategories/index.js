f0990d8c0884870591d5c7a3776289d6725cd3d4
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-07-21 16:46:29 +0200

    new changes

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,18 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Category } from "../Category";
 import { List, Item } from "./styles";
 import { categories } from '../../../api/db.json';
 import db from '../../../api/db.json'
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
 
+function useCategoriesData() {
+    const [categories, setCategories] = useState([])
+    const [loading, setLoading] = useState(false)
+    useEffect(function () {
+        setLoading(true)
+        window.fetch('https://petgram-server-mszjar.vercel.app/categories')
+            .then(res => res.json())
+            .then(response => {
+                setCategories(response)
+                setLoading(false)
+            })
+    }, [])
+
+    return { categories, loading }
+
+}
 export const ListOfCategories = () => {
-    const [categories, setCategories] = useState(db.categories)
-    useEffect
-    return (
-        <List>
+    const { categories, loading }= useCategoriesData()
+    const [showFixed, setShowFixed] = useState (false)
+
+    useEffect(function () {
+        const onScroll = e => {
+            const newShowFixed = window.scrollY > 200
+            showFixed != newShowFixed && setShowFixed(newShowFixed) 
+        }
+
+        document.addEventListener('scroll',onScroll)
+
+        return () => document.removeEventListener('scroll', onScroll)
+    }, [showFixed])
+
+    const renderList = (fixed) => (
+        <List fixed= {fixed}>
             {
-                db.categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
+                loading
+                    ? <Item key='loading'>
+                        <div> <AiOutlineLoading3Quarters /> </div>
+                    </Item>
+                    : db.categories.map(category => <Item key={category.id}><Category {...category}/></Item>)
             }
         </List>
+    )
+
+    return (
+        <Fragment>
+
+            {renderList()}
+            {showFixed && renderList(true)}
+
+        </Fragment>
+
     )
 }
