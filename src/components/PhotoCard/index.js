28f87cdc8cd4b4565ab55fcbe73894ea28981693
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-07-23 14:38:54 +0200

    new changes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,20 +1,44 @@
-import React from "react";
-import { ImgWrapper, Img, Button } from "./styles";
-import { FcLike } from 'react-icons/fc';
-
+import React, {Fragment, useEffect, useRef, useState} from "react";
+import { Article, ImgWrapper, Img, Button } from "./styles";
+import { FcLike, FcLikePlaceholder } from 'react-icons/fc';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { useNearScreen } from "../../hooks/useNearScreen";
 
 const DEFAULT_IMAGE = "https://images.unsplash.com/photo-1500879747858-bb1845b61beb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+
+
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+    const [show, element] = useNearScreen()
+    const key = `like-${id}`
+    const [liked, setLiked] = useLocalStorage(key, false)
+
+    console.log(liked)
+
+    const Icon = liked ? FcLike : FcLikePlaceholder
+
+    const setLocalStorage = value => {
+        try {
+            window.localStorage.setItem(key, value)
+            setLiked(value)
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
     return (
-        <article>
-            <a href={`/detail/${id}`}>
-                <ImgWrapper> 
-                    <Img src={src} />
-                </ImgWrapper>
-            </a>
-            <Button>
-                <FcLike size='32px' /> {likes} likes!
-            </Button>
-        </article>
+        <Article ref={element}>
+            {
+                show && <Fragment>
+                    <a href={`/detail/${id}`}>
+                        <ImgWrapper> 
+                            <Img src={src} />
+                        </ImgWrapper>
+                    </a>
+                    <Button onClick={() => setLiked(!liked)}>
+                        <Icon size='32px' /> {likes} likes!
+                    </Button>
+                </Fragment>
+            }
+        </Article>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
-import React from 'react';
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app')); */
+
+import React from 'react';
+import { App } from './App'
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container);
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'; 
+root.render(<App />);
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider
+  } from '@apollo/client'
+
+  const client = new ApolloClient({
+    uri: 'https://curso-platzi-react-avanzado-git-master-mszjar.vercel.app/',
+    cache: new InMemoryCache()
+  })
+
+  root.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById('app')
+  )
