6f39c5a785e397c1a0c2d5899f08bb3b793407d4
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-08-11 22:06:44 +0200

    useInputValue typing error fixed

--- src/hooks/useInputValue.js
+++ src/hooks/useInputValue.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export const useInputValue = initialValue => {
     const [value, setValue] = useState(initialValue)
-    const onChange = e => setValue(e.target.valye)
+    const onChange = e => setValue(e.target.value)
 
     return { value, onChange }
 }
