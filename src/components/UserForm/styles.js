8e209e156992de7fbcb91b70953f2d2067637298
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-08-12 08:53:47 +0200

    styles components fixed

--- src/components/UserForm/styles.js
+++ src/components/UserForm/styles.js
@@ -6,13 +6,13 @@ export const Form = styled.form`
 export const Input = styled.input`
     border: 1px solid #ccc;
     border-radius: 3px;
-    margin-botton: 8px;
+    margin-bottom: 8px;
     padding: 8px 4px;
     display: block;
     width: 100%;
 `
 export const Button = styled.button`
-    background: #8d00ff;'
+    background: #8d00ff;
     border-radius: 3px;
     color: #fff;
     height: 32px;
