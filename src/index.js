f2c5becb851a33f66b554f1fe77b41861168b136
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-08-13 12:27:26 +0200

    Apollo client fixed

--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import { App } from './App'
 import { createRoot } from 'react-dom/client';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, ApolloLink, from, HttpLink, setContext, operationName } from '@apollo/client';
 import Context from './Context';
-import { setContext } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
 
-const client = new ApolloClient({
-    uri: 'https://petgram-server-mszjar-qury9j4md-mszjar.vercel.app/graphql',
-    cache: new InMemoryCache(),
-    request: operation => {
-      const token = window.sessionStorage.getItem('token')
-      const authorization = token ? `Bearer ${token}` : ''
-      operation.setContext({
-        headers: {
-          authorization
-        }
-      })
-    },
-    onError: error => {
-      const { networkError } = error
-      if (networkError && networkError.result.code === 'invalid_token') {
-        window.sessionStorage.removeItem('token')
-        window.location.href = '/'
-      }
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = sessionStorage.getItem('token');
+  const authorization = token ? `Bearer ${token}` : ''
+  operation.setContext({
+    headers: {
+      authorization
     }
+  })
+  return forward(operation)
+});
+
+const errorMiddleware = onError(
+  ({ networkError }) => {
+    if (networkError && networkError.result.code === 'invalid_token') {
+      sessionStorage.removeItem('token');
+      window.location = '/';
+    }
+  }
+);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: from([
+    errorMiddleware,
+    authMiddleware,
+    new HttpLink({
+      uri: 'https://petgram-server-mszjar-qury9j4md-mszjar.vercel.app/graphql', 
+    })
+  ])
 });
 
 root.render(
