689e885e6bdaf958eac6d4e2a6823619bb6d1820
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-08-14 00:23:36 +0200

    Performances improvements added

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -22,7 +22,8 @@ function useCategoriesData() {
     return { categories, loading }
 
 }
-export const ListOfCategories = () => {
+
+const ListOfCategoriesComponent = () => {
     const { categories, loading }= useCategoriesData()
     const [showFixed, setShowFixed] = useState (false)
 
@@ -59,3 +60,5 @@ export const ListOfCategories = () => {
 
     )
 }
+
+export const ListOfCategories = React.memo(ListOfCategoriesComponent)
