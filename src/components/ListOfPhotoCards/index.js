9754a69d71a93eab47bc29d8243a56e02f43b1a9
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-08-13 13:36:44 +0200

    favorites page added

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import { PhotoCard } from '../PhotoCard'
 import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
 import { GET_PHOTOS } from '../../hoc/get_photos';
+import { Spinner } from "../../styles/animation";
 
 
 export const ListOfPhotoCards = ({ categoryId }) => {
     const { loading, error, data } = useQuery(GET_PHOTOS, { variables: { categoryId: categoryId } })
-    if (loading) return <h2>Loading...</h2>
+    if (loading) return <Spinner />
     if (error) return <h2>Error!</h2>
     return (
       <ul>
