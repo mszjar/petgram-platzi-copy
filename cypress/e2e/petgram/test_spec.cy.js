b2077b319120cb485041228f1ff384a4b0362187
Mario Sanchez Jaramillo <mario.sanchez.jar@gmail.com>
2022-08-14 17:59:01 +0200

    Cleaning some coments

--- cypress/e2e/petgram/test_spec.cy.js
+++ cypress/e2e/petgram/test_spec.cy.js
@@ -21,25 +21,3 @@ describe('Petgram', function () {
         cy.get('form').should('have.length', 2);
     })
 })
-
-/* describe('Petgram', function () {
-    it('para ver si funciona', function () {
-        cy.visit('https://petgram-copy-git-main-mszjar.vercel.app/');
-    })
-
-    it('navegamos a una categoria y vemos fotos', function() {
-        cy.visit('https://petgram-copy-git-main-mszjar.vercel.app/pet/1');
-        cy.get('article');
-    });
-
-    it('si podemos navegar con la navbar a la home',  function() {
-        cy.visit('https://petgram-copy-git-main-mszjar.vercel.app/pet/1');
-        cy.get('nav a').first().click;
-        cy.url().should('include', '/');
-    })
-
-    it('los usuarios no registrados ven el formulario de registro e inicio de sesion al ir a favs', function() {
-        cy.visit('https://petgram-copy-git-main-mszjar.vercel.app/favs');
-        cy.get('form').should('have.length', 2);
-    })
-}) */
